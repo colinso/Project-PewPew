66a3793dd5d4242553854b774d06209e5a16dd48
Kathy <sapphirecloud15@gmail.com>
2019-08-26 11:29:55 -0600

    Start on player and enemy stuffs

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health;
+    public GameObject ammoDrop;
+    public int experienceDrop;
+    public int damage;
+    public int speed;
+    public string weakness;
+    public int weaknessMultiplier;
+    public int distanceFromPlayer;
+    public Rigidbody2D rb;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPosition = player.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
+
+    }
+
+    public void takeDamage(int damageTaken)
+    {
+        health -= damageTaken;
+    }
+
+    public void inflictDamage(int damageInflicted)
+    {
+        player.GetComponent<PlayerController>().takeDamage(damageInflicted);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int health;
+    public int stamina;
+    public int experience;
+    public int level;
+    public GameObject currentWeapon;
+    public List<GameObject> weapons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damageTaken)
+    {
+        health -= damageTaken;
+    }
+    public void inflictDamage(int damageInflicted)
+    {
+        
+    }
+}
