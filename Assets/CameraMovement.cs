7f6bc7c3acab24f3312afc0a09cb7035e61de26f
Kathy <sapphirecloud15@gmail.com>
2019-08-26 11:48:48 -0600

    Add camera movement and take out library files

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
