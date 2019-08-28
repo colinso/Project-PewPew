4485f03f53e29a1fa2d63095b33cc0b2c3c00eb6
Kathy <sapphirecloud15@gmail.com>
2019-08-28 15:09:03 -0600

    Door functionality and lighting

---
+++ Assets/Scripts/SceneManagement/StartPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+    public int startPointNumber;
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+}
